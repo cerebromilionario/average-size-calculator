d2f0b304bbe41e79b4fb39c1347e99ee09cc480e
cerebromilionario <will-dc@hotmail.com>
2026-05-24 17:56:20 -0300

    Configure production domain

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
   name: 'Average Size Calculator',
-  // TODO: Replace with the canonical production domain when available.
-  siteUrl: 'https://average-size-calculator.netlify.app',
+  siteUrl: 'https://averagesizecalculator.site',
   description:
     'A private educational size comparison tool that helps users compare measurements with global and country average data.'
 };
